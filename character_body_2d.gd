35fd4461cae5f2edefe6c3203863ba0cd79c8942
zaerald <zaerald.zd@gmail.com>
2026-01-30 18:07:55 +0800

    feat: animate player

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -6,6 +6,13 @@ func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
