e0da931af236c1999d95a5e1b28561b0db1905d7
0R1862 <reo06618@gmail.com>
2026-02-01 00:36:18 +0800

    add sound

--- enemy_th.gd
+++ enemy_th.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
+@onready var gameover: AudioStreamPlayer2D = $"../../Gameover"
+
 func _on_enemy_th_body_entered(body):
 	if body.is_in_group("Player"):
 		hide()
 		await get_tree().create_timer(0.3).timeout
+		gameover.play()
+		await get_tree().create_timer(1.85).timeout
 		get_tree().paused = true
 		await get_tree().create_timer(3).timeout
 		get_tree().quit()
